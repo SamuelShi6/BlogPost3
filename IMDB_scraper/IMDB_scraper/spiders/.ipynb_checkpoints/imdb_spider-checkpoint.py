266375229ed4805ba8e00eb3b43bc40815ad4e31
SamuelShi6 <97071132+SamuelShi6@users.noreply.github.com>
2022-02-03 00:04:33 -0800

    created the list

---
+++ IMDB_scraper/IMDB_scraper/spiders/.ipynb_checkpoints/imdb_spider-checkpoint.py
@@ -0,0 +1,31 @@
+# to run
+# scrapy crawl imdb_spider -o movies.csv
+
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+
+    start_urls = ['https://www.imdb.com/title/tt0330373/']
+
+    def parse(self, response):
+        navigation_link = response.url + 'fullcredits/'
+        yield scrapy.Request(navigation_link, callback = self.parse_full_credits)
+
+    def parse_full_credits(self, response):
+        actor_list = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        for actor in actor_list:
+            yield scrapy.Request('https://www.imdb.com'+actor, callback = self.parse_actor_page)
+
+    def parse_actor_page(self, response):
+        rough_film_list = response.css("div.filmo-row").css("a::text").getall()
+        exact_list = [ i for i in rough_film_list if i[0:7] != 'Episode']
+        actor_name = response.css("span.itemprop::text").get()
+        
+        for film in exact_list:
+            yield {
+                "actor": actor_name,
+                "movie_or_TV_name": film
+            }
+        
+
