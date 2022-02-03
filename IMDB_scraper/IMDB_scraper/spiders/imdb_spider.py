612ae45f34bc0d1a4516e4df61884d3ad02285c9
SamuelShi6 <97071132+SamuelShi6@users.noreply.github.com>
2022-02-03 00:28:49 -0800

    removed show all

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -6,7 +6,7 @@ import scrapy
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
 
-    start_urls = ['https://www.imdb.com/title/tt0106145/']
+    start_urls = ['https://www.imdb.com/title/tt0330373/']
 
     def parse(self, response):
         navigation_link = response.url + 'fullcredits/'
@@ -19,7 +19,7 @@ class ImdbSpider(scrapy.Spider):
 
     def parse_actor_page(self, response):
         rough_film_list = response.css("div.filmo-row").css("a::text").getall()
-        exact_list = [ i for i in rough_film_list if i[0:7] != 'Episode']
+        exact_list = [ i for i in rough_film_list if i[0:7] != 'Episode' and i[0:8] != 'Show all']
         actor_name = response.css("span.itemprop::text").get()
         
         for film in exact_list:
